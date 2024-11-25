823c73d673390acd30d1d7f16caabfaf4a771b0d
NavjotSingh02 <navjot.singh02@edu.sait.ca>
2024-11-24 19:18:39 -0700

    lab4

--- app/index.js
+++ app/index.js
@@ -1,34 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
 
-export default function Page() {
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+  const addTask = (task) => {
+    // Implement the logic to add a new task
+    setTasks([...tasks, task]);
+
+  };
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <SafeAreaView>
+      <ToDoList tasks={tasks}/>
+      <ToDoForm addTask={addTask} />
+      
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+  // Styles can be removed from here as they are already defined in the respective components
 });
+
+export default App;
